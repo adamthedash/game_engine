2d096921ae166673aa31e43dafe058c9292170e5
Adam Power <adamthedash2@gmail.com>
2025-07-11 23:05:57 +0100

    Remove world saving/loading for now

--- game_engine/src/bin/game.rs
+++ game_engine/src/bin/game.rs
@@ -148,7 +148,7 @@ impl ApplicationHandler for App {
 
     fn exiting(&mut self, _event_loop: &ActiveEventLoop) {
         println!("Saving world...");
-        self.game_state.world.save(Path::new("./saves"));
+        //self.game_state.world.save(Path::new("./saves"));
     }
 
     fn device_event(
